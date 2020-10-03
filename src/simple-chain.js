3275a3f8225a20d552a859efaf11db1a29a78a1b
Kairat Satbekov <kairat.satbekov@Kairats-MacBook-Pro.local>
2020-10-03 06:59:18 +0600

    add solution to simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  list: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.list.length;
   },
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.list.push(value);
+    return this;
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(position === '' || this.list[position] === undefined || isNaN(+position)) {
+      this.list = [];
+      throw new Error('Error');
+    }
+    this.list.splice(position - 1, 1);
+    return this;
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+   this.list.reverse();
+   return this;
   },
+
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let resultList = '';
+    this.list.map((item, index) => {
+      resultList += (index !== 0) ? `~~( ${item} )` : `( ${item} )`;
+    })
+    this.list = [];
+    return resultList;
   }
 };
 
