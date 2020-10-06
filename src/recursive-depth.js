fc6f84aa4c80496d1843dec3d575d8ff0ca0457c
Kairat Satbekov <kairat.satbekov@Kairats-MacBook-Pro.local>
2020-10-07 01:59:08 +0600

    add solution to recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,7 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(arr) {
-
+  calculateDepth(arr, depth = 1) {
+    return arr.reduce((accum, currValue) => {
+      if (Array.isArray(currValue)) {
+        return Math.max(this.calculateDepth(currValue, depth + 1), accum);
+      }
+      return accum;
+    }, depth)
   }
 };
