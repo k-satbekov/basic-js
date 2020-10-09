d1a588700a4523ce1137f89b10646f0b8d59ca27
Kairat Satbekov <kairat.satbekov@Kairats-MacBook-Pro.local>
2020-10-09 17:22:46 +0600

    add solution to extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let res = '';
+
+  (options.repeatTimes === undefined) ? options.repeatTimes = 1 : null;
+
+  for (let i = 0; i < options.repeatTimes; i++) {
+    res += str;
+    if (options.addition !== undefined) {
+      if(options.additionRepeatTimes) {
+        for (let j = 0; j < options.additionRepeatTimes; j++) {
+          (j === options.additionRepeatTimes - 1) ? res += options.addition : res += options.addition + options.additionSeparator;
+        }
+      } else {
+        res += options.addition;
+      }
+    }
+    if (options.separator === undefined) {
+      options.separator = '+';
+    }
+    if (i < options.repeatTimes - 1) {
+      res += options.separator;
+    }
+  }
+
+  return res;
 };
   
